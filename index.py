d86f2d777a2ebf112a53264f42888d9bbee8aa05
jubaer.sookh <jubaer@sookh.com>
2022-05-17 12:15:30 +0600

    selection sort

--- index.py
+++ index.py
@@ -1,16 +1,22 @@
-#bubble sort
+#Selection Sort
 
-def bubble_sort(data):
-    for i in range(len(data)):
-        for j in range(len(data)-1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
-    return data
-
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if(arr[j]< arr[min_index]):
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
 if __name__ == '__main__':
+    arr = [5,2,4,6,1,3]
+    print(selectionSort(arr))
 
-    data = [3, 5, 1, 4, 2]
-    print(bubble_sort(data))
-    
+
+    #time complexity: O(n^2) because of nested loops and swaps in the inner loop 
+    #space complexity: O(1) because no extra space is used 
+
+
+
 
