08d273a519b13ffb94cee46bf1024c4810ac84d8
jubaer.sookh <jubaer@sookh.com>
2022-05-17 11:44:11 +0600

    linear search

--- index.py
+++ index.py
@@ -1,35 +1,12 @@
-#python hash table
-hash_table = [0] * 10
-def checkPrime(num):
-    if num == 1 or num == 0:
-        return False
-    for i in range(2, num//2):
-        if num % i == 0:
-            return False
-    return True
-def getPrime(n):
-    if n % 2 == 0:
-        n += 1
-    while not checkPrime(n):
-        n += 2
-    return n
-def hashFunction(key):
-    c = getPrime(10)
-    return key % c
+#linear search
 
-def insert_hash(key, value):
-    index = hashFunction(key)
-    hash_table[index] = [key, value]
-def remove_hash(key):
-    index = hashFunction(key)
-    hash_table[index] = 0
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def find_linear(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            return i
+    return -1
 
 
-insert_hash(1, "one")
-insert_hash(2, "two")
-insert_hash(3, "three")
-insert_hash(4, "four")
-insert_hash(5, "five")
-
-
-print(hash_table)
+print(find_linear(data, 5))
