bb5565b1a1e75578a97aa1867a1dc77655618381
jubaer.sookh <jubaer@sookh.com>
2022-05-17 19:09:52 +0600

    quick sort

--- index.py
+++ index.py
@@ -1,44 +1,60 @@
-#Merge Sort
-def MergeSort(data):
-    print(data)
-    if len(data) > 1:
-        mid = len(data) // 2
-        L = data[:mid]
-        R = data[mid:]
+#quick sort
 
-        MergeSort(L)
-        MergeSort(R)
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                data[k] = L[i]
-                i += 1
-            else:
-                data[k] = R[j]
-                j += 1
-            k += 1
-
-        while i < len(L):
-            data[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            data[k] = R[j]
-            j += 1
-            k += 1
+from dataclasses import dataclass
 
 
+def quickSort(data):
+    first = 0
+    last = len(data) -1
 
-data = [54,26,93,17,77,31,44,55,20]
-MergeSort(data)
+    quickSortHelper(data,first,last)
+
+
+def quickSortHelper(data,first,last):
+    # print (data,first,last)
+    if first < last:
+
+        dividedPoint = partition(data,first,last)
+        # print(data,first,dividedPoint-1)
+        quickSortHelper(data,first,dividedPoint-1)
+        quickSortHelper(data,dividedPoint+1,last)
+
+
+def partition(data,first,last):
+    pivotvalue = data[first]
+    # print("pivotvalue",pivotvalue)
+    leftmark = first+1
+    rightmark = last
+
+    # print("leftmark",leftmark)
+    # print("rightmark",rightmark)
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and data[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+            # print('lol',leftmark)
+
+        while data[rightmark] >= pivotvalue and rightmark >= leftmark:
+            # print('lol2',rightmark)
+            rightmark = rightmark -1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = data[leftmark]
+            data[leftmark] = data[rightmark]
+            data[rightmark] = temp
+            # print('else',data)
+
+    temp = data[first]
+    data[first] = data[rightmark]
+    data[rightmark] = temp
+    # print('temp',data)
+
+
+    return rightmark
+
+data = [29,10,14,37,15]
+quickSort(data)
 print(data)
-
-
-#best case: O(n)
-#worst case: O(n^2)
-#average case: O(n log n)
-#space complexity: O(n)
-
