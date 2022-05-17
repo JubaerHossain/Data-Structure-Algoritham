4b171e965c4cb1b3810e2efe8e0333bc85a2bb5e
jubaer.sookh <jubaer@sookh.com>
2022-05-17 11:50:04 +0600

    binary Search

--- index.py
+++ index.py
@@ -1,12 +1,31 @@
-#linear search
+#Binary Search
+
+def find_binary(data, item):
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = data[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
 
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def find_linear(data, target):
-    for i in range(len(data)):
-        if data[i] == target:
-            return i
-    return -1
+input = int(input("Enter a number to search for: "))
+
+target = find_binary(data, input)
+
+print("Searching for {} in {}".format(input, data))
+
+print("Found {} at index {}".format(input, target))
 
 
-print(find_linear(data, 5))
+
+
+
+
