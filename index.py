18766cea4802ebfca8ac80579ac4483d0e2f058a
jubaer.sookh <jubaer@sookh.com>
2022-05-17 12:24:46 +0600

    insertion sort

--- index.py
+++ index.py
@@ -1,21 +1,17 @@
-#Selection Sort
+#Insertion Sort
+def insertionSort(data):
+    for i in range(1,len(data)):
+        j = i
+        while j > 0 and data[j] < data[j-1]:
+            data[j], data[j-1] = data[j-1], data[j]
+            j = j-1
+    return data
 
-def selectionSort(arr):
-    for i in range(len(arr)):
-        min_index = i
-        for j in range(i+1, len(arr)):
-            if(arr[j]< arr[min_index]):
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+data = [5,2,4,6,1,3]
+print(insertionSort(data))
 
-if __name__ == '__main__':
-    arr = [5,2,4,6,1,3]
-    print(selectionSort(arr))
-
-
-    #time complexity: O(n^2) because of nested loops and swaps in the inner loop 
-    #space complexity: O(1) because no extra space is used 
+#time complexity: O(n^2) because of nested loops and swaps in the inner loop 
+#space complexity: O(1) because no extra space is used 
 
 
 
